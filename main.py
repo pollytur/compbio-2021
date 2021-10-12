d02cbd9db0613725c7b2caf7e86010e3451e82c8
Nikolay Tverdokhleb <48504887+IamNikolayTverdokhleb@users.noreply.github.com>
2021-10-12 11:34:53 +0200

    Update search algo
    Check it :)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from collections import Counter
 if __name__ == '__main__':
     path = "salmonella-enterica.reads.fna"
     fasta_sequences = SeqIO.parse(path, "fasta")
-    window = 5
+    window = 10
     small_seqs = []
     sequences = []
     for fasta in fasta_sequences:
@@ -16,23 +16,28 @@ if __name__ == '__main__':
 
     fasta_counter = Counter(small_seqs)
     n = 50
-    m = 500
+    m = 50000
+    sim_counter = 0
     n_least_common = fasta_counter.most_common()[:-n-1:-1]
-    m_most_common = fasta_counter.most_common(m)
-
+    m_most_common = fasta_counter.most_common(m)[1:m]
+    print(m_most_common)
     for rare in n_least_common:
         for popular in m_most_common:
             similarity_counter = 0
-            if popular == "":
+
+            if len(popular[0]) < window or len(rare[0]) < window:
                 continue
-            print("Rare = ", rare)
-            print("Popular = ", popular)
+
             for i in range(window):
-                if(rare[i] == popular[i]):
+                if rare[0][i] == popular[0][i]:
                     similarity_counter += 1
+                #print("similarity_counter = ", similarity_counter)
 
             if similarity_counter == window - 1:
                 print("Rare = ", rare)
                 print("Popular = ", popular)
+                sim_counter += 1
+
+    print("sim_counter = ", sim_counter)
 
 
